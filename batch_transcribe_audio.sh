a07018137e88188ebdf1fa6754ddd6729380c143
Zach Hedeen <hedeenza@gmail.com>
2026-05-23 22:02:35 -0400

    added the batch audio file processing Bash script, added a line to get each file name added to the combined transcript

--- batch_transcribe_audio.sh
+++ batch_transcribe_audio.sh
@@ -25,30 +25,26 @@ NUMBER=1
 for i in ./*.$1; do
 
     # Assign the file for this iteration to the video variable
-    VIDEO=$i
-    # Create a temporary audio file
-    AUDIO="audio_out."$1
+    AUDIO=$i
     # Create a transcript file from the video file name
-    TEXT=${VIDEO%.*}".txt"
+    TEXT=${AUDIO%.*}".txt"
 
     # Notify the user which file is being processed
     # and how progress is going overall
-    echo "Processing ${VIDEO#./*} (${NUMBER}/${#FILES[@]})..."
+    echo "Processing ${AUDIO#./*} (${NUMBER}/${#FILES[@]})..."
 
     # Create an audio file from the video file with ffmpeg
     ffmpeg -hide_banner -loglevel error -i $VIDEO -vn -acodec copy $AUDIO
 
     # Source the virtual environment
     source /home/user/path/to/where/the/venv/lives/venv/bin/activate
-    # Add the name of the video file to the combined transcript
-    echo $VIDEO >> $BATCH_TEXT
+    # Add the name of the audio file to the combined transcript
+    echo $AUDIO >> $BATCH_TEXT
     # Run the speech_to_text script
     python3 /home/user/path/to/where/the/script/lives/speech_to_text.py $AUDIO | tee $TEXT >> $BATCH_TEXT
-
-    # Remove the temporary audio file
-    rm $AUDIO
 
     # Increment the counter
     ((NUMBER++))
 
 done
+    
